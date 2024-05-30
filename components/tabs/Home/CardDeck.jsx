4b18af43add23e3ab162905343817d6be627b31e
Jose Kim <barsamic03@gmail.com>
2024-05-31 00:21:45 +0900

    Get card imageURL and name from prop data

--- components/tabs/Home/CardDeck.jsx
+++ components/tabs/Home/CardDeck.jsx
@@ -14,10 +14,8 @@ export default function CardDeck({ cards, swipeRightFn }) {
           height={"90%"}
           width={"100"}
           gradientHeight={0.6}
-          imageURL={
-            "https://images.unsplash.com/photo-1493612276216-ee3925520721?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tfGVufDB8fDB8fHww"
-          }
-          name={"Name"}
+          imageURL={card.ImageURL}
+          name={card.ProductTitle}
           nameSize={36}
         />
       )}

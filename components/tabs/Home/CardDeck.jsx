fb94227ece31a32eefb32f8e24204229e86b931f
Jose Kim <barsamic03@gmail.com>
2024-05-30 23:04:06 +0900

    Pass ItemCard to CardDeck with arbitrary info

--- components/tabs/Home/CardDeck.jsx
+++ components/tabs/Home/CardDeck.jsx
@@ -1,11 +1,30 @@
 import Swiper from "react-native-deck-swiper";
 import { useState } from "react";
 import { StyleSheet } from "react-native";
+import ItemCard from "../_common/ItemCard";
 
 export default function CardDeck() {
   const [cards, setCards] = useState([]);
 
-  return <Swiper cards={cards} />;
+  return (
+    <Swiper
+      cards={cards}
+      renderCard={(card) => (
+        <ItemCard
+          height={"90%"}
+          width={"100"}
+          gradientHeight={0.6}
+          imageURL={
+            "https://images.unsplash.com/photo-1493612276216-ee3925520721?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tfGVufDB8fDB8fHww"
+          }
+          name={"Name"}
+          nameSize={36}
+        />
+      )}
+      cardVerticalMargin={0}
+      cardHorizontalMargin={0}
+    />
+  );
 }
 
 const styles = StyleSheet.create({

65ce0fe3f679e21857d4c5669bd86b613246389c
Jose Kim <barsamic03@gmail.com>
2024-05-30 23:02:37 +0900

    Add styling to CardDeck

--- components/tabs/Home/CardDeck.jsx
+++ components/tabs/Home/CardDeck.jsx
@@ -1,8 +1,18 @@
 import Swiper from "react-native-deck-swiper";
 import { useState } from "react";
+import { StyleSheet } from "react-native";
 
 export default function CardDeck() {
   const [cards, setCards] = useState([]);
 
   return <Swiper cards={cards} />;
 }
+
+const styles = StyleSheet.create({
+  swiperContainer: {
+    // marginTop: 50,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgb(255, 255, 247)",
+  },
+});

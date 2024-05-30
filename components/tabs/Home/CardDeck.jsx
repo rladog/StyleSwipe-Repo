47afeb7aa7b710b99f0b2290e9daa9496895b44a
Jose Kim <barsamic03@gmail.com>
2024-05-30 21:45:39 +0900

    Import useState for CardDeck to store card data

--- components/tabs/Home/CardDeck.jsx
+++ components/tabs/Home/CardDeck.jsx
@@ -1,5 +1,8 @@
 import Swiper from "react-native-deck-swiper";
+import { useState } from "react";
 
 export default function CardDeck() {
-  return <Swiper />;
+  const [cards, setCards] = useState([]);
+
+  return <Swiper cards={cards} />;
 }

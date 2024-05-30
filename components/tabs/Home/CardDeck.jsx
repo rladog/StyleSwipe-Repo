0ae947dd8203ab7dd448494c26515c29c5aaec88
Jose Kim <barsamic03@gmail.com>
2024-05-30 23:40:45 +0900

    Make CardDeck render conditionally

--- components/tabs/Home/CardDeck.jsx
+++ components/tabs/Home/CardDeck.jsx
@@ -7,27 +7,31 @@ export default function CardDeck({ cards, swipeRightFn }) {
   const [data, setData] = useState(cards);
 
   return (
-    <Swiper
-      cards={data}
-      renderCard={(card) => (
-        //Render the cards using the ItemCard component
-        //Gradient height should be a fraction between 0 and 1 based on how fast the gradient should start
-        <ItemCard
-          height={"90%"}
-          width={"100"}
-          gradientHeight={0.6}
-          imageURL={
-            "https://images.unsplash.com/photo-1493612276216-ee3925520721?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tfGVufDB8fDB8fHww"
-          }
-          name={"Name"}
-          nameSize={36}
+    <>
+      {data && (
+        <Swiper
+          cards={data}
+          renderCard={(card) => (
+            //Render the cards using the ItemCard component
+            //Gradient height should be a fraction between 0 and 1 based on how fast the gradient should start
+            <ItemCard
+              height={"90%"}
+              width={"100"}
+              gradientHeight={0.6}
+              imageURL={
+                "https://images.unsplash.com/photo-1493612276216-ee3925520721?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tfGVufDB8fDB8fHww"
+              }
+              name={"Name"}
+              nameSize={36}
+            />
+          )}
+          cardVerticalMargin={0}
+          cardHorizontalMargin={0}
+          onSwipedRight={(index) => swipeRightFn(index)}
+          verticalSwipe={false}
         />
-      )}
-      cardVerticalMargin={0}
-      cardHorizontalMargin={0}
-      onSwipedRight={(index) => swipeRightFn(index)}
-      verticalSwipe={false}
-    />
+      )}{" "}
+    </>
   );
 }
 

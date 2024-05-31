9c67c3d64fbeec63e519c4fef03b5ce39048d67a
Jose Kim <barsamic03@gmail.com>
2024-05-31 13:35:33 +0900

    Create hook for getting user session info

---
+++ hooks/useSession.js
@@ -0,0 +1,23 @@
+import "react-native-url-polyfill/auto";
+import { useState, useEffect, useCallback } from "react";
+import { supabase } from "@/utils/supabase";
+import Auth from "@/components/tabs/_common/Auth";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { View } from "react-native";
+
+export default function useSession() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, [session]);
+
+  return { sessionExists: session != null, session };
+}

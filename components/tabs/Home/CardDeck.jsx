b3c693d67593ef2842aab34baa1a27fa372d6237
Jose Kim <barsamic03@gmail.com>
2024-06-01 13:55:21 +0900

    Pass function to be called on double tapping card

--- components/tabs/Home/CardDeck.jsx
+++ components/tabs/Home/CardDeck.jsx
@@ -3,7 +3,15 @@ import { useState } from "react";
 import { StyleSheet } from "react-native";
 import ItemCard from "../_common/ItemCard";
 
-export default function CardDeck({ cards, swipeRightFn, swipeLeftFn }) {
+export default function CardDeck({
+  cards,
+  swipeRightFn,
+  swipeLeftFn,
+  doubleTapFn,
+}) {
+  //State for managing if the card has been double tapped or not
+  const [taps, setTaps] = useState(0);
+
   return (
     <Swiper
       cards={cards}
@@ -31,6 +39,25 @@ export default function CardDeck({ cards, swipeRightFn, swipeLeftFn }) {
       //with the given index of the card
       onSwipedRight={(index) => swipeRightFn(cards[index])}
       onSwipedLeft={(index) => swipeLeftFn(cards[index])}
+      onTapCard={(index) => {
+        //On the first tap, set number of taps to 1
+        if (taps == 0) {
+          setTaps(1);
+          return;
+        }
+        //On the second tap,
+        //call the double tap function
+        //and set the number of taps to zero
+        if (taps == 1) {
+          doubleTapFn(cards[index]);
+          setTaps(0);
+        }
+      }}
+      //Reset the tap counter
+      //if the card has been moved by more than 50 units
+      onSwiping={(x, y) => {
+        if (x > 50 || x < -50) setTaps(0);
+      }}
       containerStyle={styles.swiperContainer}
     />
   );

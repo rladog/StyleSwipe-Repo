1d0f206b2333ad4bca7b58e23d91e49547f913c3
Jose Kim <barsamic03@gmail.com>
2024-06-03 01:00:42 +0900

    Modify Collections tab to use useFocusEffect instead

--- app/(tabs)/collections.jsx
+++ app/(tabs)/collections.jsx
@@ -1,16 +1,22 @@
 import Collections from "../../components/tabs/Collections/Collections";
 import useSession from "@/hooks/useSession";
 import Auth from "@/components/tabs/_common/Auth";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import getCollections from "@/utils/getCollections";
+import { useFocusEffect } from "expo-router";
 
 export default function CollectionScreen() {
   const { sessionExists, session } = useSession();
   const [collections, setCollections] = useState(null);
 
-  useEffect(() => {
-    getCollections(session).then((collections) => setCollections(collections));
-  }, [session]);
+  useFocusEffect(
+    useCallback(() => {
+      getCollections(session).then((collections) =>
+        setCollections(collections)
+      );
+      console.log("hi");
+    }, [session])
+  );
 
   if (!sessionExists) return <Auth />;
   return <Collections collectionsProp={collections} />;

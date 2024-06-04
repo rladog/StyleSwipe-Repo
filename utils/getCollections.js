7bde64f53a21b7e6a0668d624e03038d88d99860
Jose Kim <barsamic03@gmail.com>
2024-06-04 23:49:30 +0900

    Remove session info argument from getCollections

--- utils/getCollections.js
+++ utils/getCollections.js
@@ -1,12 +1,22 @@
+import "react-native-url-polyfill/auto";
 import { supabase } from "@/utils/supabase";
+import getSession from "@/utils/getSession";
 
-export default async function getCollections(session) {
-  if (!session) return null;
+export default async function getCollections() {
+  let session = await getSession();
+
+  if (!session) {
+    alert("Error getting login info");
+    console.log(error);
+    return null;
+  }
+
+  let user_id = session.data.session.user.id;
 
   const { data, error } = await supabase
     .from("collections")
     .select("collection_obj")
-    .eq("user_id", session?.user.id)
+    .eq("user_id", user_id)
     .single();
 
   if (error) {
@@ -16,6 +26,7 @@ export default async function getCollections(session) {
   }
 
   if (data) {
+    console.log(data);
     return data?.collection_obj;
   }
 }

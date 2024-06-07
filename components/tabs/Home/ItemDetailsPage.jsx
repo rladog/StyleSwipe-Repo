93daf45999c74a361eefd1d4ea0c8b55b4c6f10b
Jose Kim <barsamic03@gmail.com>
2024-06-07 16:56:16 +0900

    Create full-screen component for item details

---
+++ components/tabs/Home/ItemDetailsPage.jsx
@@ -0,0 +1,116 @@
+import { StyleSheet, View, Text, Image, Modal, Pressable } from "react-native";
+import useFontImport from "@/hooks/useFontImport";
+import DetailedItemCard from "@/components/tabs/_common/DetailedItemCard";
+import CollectionMenu from "@/components/tabs/_common/CollectionMenu";
+import getCollections from "@/utils/getCollections";
+import createCollectionAndAdd from "@/utils/createCollectionAndAdd";
+import addItemToCollection from "@/utils/addItemToCollection";
+import { useEffect, useState } from "react";
+import LoadingScreen from "@/components/tabs/_common/LoadingScreen";
+
+export default function ItemDetails({ itemObj, closeFn }) {
+  const { fontsReady } = useFontImport();
+  const [showCollectionMenu, setShowCollectionMenu] = useState(false);
+  const [collections, setCollections] = useState([{}]);
+
+  useEffect(() => {
+    getCollections().then((collections) => setCollections(collections));
+  }, []);
+
+  if (!fontsReady) {
+    return <LoadingScreen loadingText={"Loading..."} />; // Render nothing while fonts are loading
+  }
+
+  return (
+    <>
+      <View style={styles.modal}>
+        <View style={styles.card}>
+          <View style={styles.buttonHolder}>
+            <Pressable style={styles.closePressable} onPress={closeFn}>
+              <Text style={styles.closeText}>Close</Text>
+            </Pressable>
+            <Pressable
+              style={styles.addPressable}
+              onPress={() => setShowCollectionMenu(true)}
+            >
+              <Text style={styles.addText}>{"Add to \ncollection"}</Text>
+            </Pressable>
+          </View>
+
+          <DetailedItemCard
+            height={"100%"}
+            width={"100%"}
+            gradientHeight={0.6}
+            imageURL={itemObj.ImageURL}
+            name={itemObj.ProductTitle}
+            nameSize={40}
+            gender={itemObj.Gender}
+            genderSize={20}
+            type={itemObj.ProductType}
+            typeSize={20}
+            subtype={itemObj.SubCategory}
+            subtypeSize={18}
+          />
+          <View>
+            <Pressable>
+              <Text>Press me!</Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+
+      <CollectionMenu
+        visible={showCollectionMenu}
+        itemId={itemObj.ProductId}
+        onClose={() => setShowCollectionMenu(false)}
+        addToCollectionFn={(itemId, collectionName) =>
+          addItemToCollection(itemId, collectionName)
+        }
+        newCollectionFn={(itemId, collectionName) =>
+          createCollectionAndAdd(itemId, collectionName)
+        }
+        collectionsObj={collections}
+      />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  modal: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "white",
+    position: "absolute",
+    zIndex: 3,
+  },
+  card: {
+    justifyContent: "flex-end",
+    alignItems: "center",
+    backgroundColor: "rgb(255, 255, 247)",
+    margin: 0,
+  },
+  buttonHolder: {
+    width: "85%",
+    position: "absolute",
+    top: "9%",
+    zIndex: 1,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  closePressable: {},
+  closeText: {
+    fontFamily: "Satoshi-Regular",
+    fontSize: 26,
+    textShadowColor: "white",
+    textShadowRadius: 5,
+  },
+  addPressable: {},
+  addText: {
+    fontFamily: "Satoshi-Regular",
+    fontSize: 22,
+    textAlign: "center",
+    textShadowRadius: 6,
+  },
+});

b983fa4f9f322f6af5b5a69f6da78ea10f54efdf
Jose Kim <barsamic03@gmail.com>
2024-06-08 16:50:30 +0900

    Implement basic createListing function

---
+++ utils/createListing.js
@@ -0,0 +1,56 @@
+import { supabase } from "@/utils/supabase";
+import getSession from "@/utils/getSession";
+import getNewItemId from "@/getNewItemId";
+import getListings from "@/getListings";
+export default async function createListing(imageURL, name, gender, category) {
+  let session = await getSession();
+
+  /*
+  ***required fields = ProductId, ImageURL, ProductTitle, Gender, ProductType, user_id
+
+  1. Get the number of items in the items table and add 60000 to that to use as ProductId
+  2. Upsert the itemObj into items
+  3. Upsert the listingsObj into listings with user_id added onto it
+  
+  */
+
+  let id = await getNewItemId();
+  let itemObj = {
+    ProductId: id,
+    ImageURL: imageURL,
+    ProductTitle: name,
+    Gender: gender,
+    ProductType: category,
+  };
+
+  const { collectionError } = await supabase
+    .from("collections")
+    .upsert(itemObj);
+
+  if (collectionError) {
+    alert("Error creating listing");
+    console.log(collectionError);
+    return false;
+  }
+
+  let user_id = session.data.session.user.id;
+
+  let ProductArray = await getListings();
+
+  ProductArray.push(itemObj);
+
+  let listingObj = {
+    user_id,
+    ProductArray,
+  };
+
+  const { listingError } = await supabase.from("listings").upsert(listingObj);
+
+  if (listingError) {
+    alert("Error creating listing");
+    console.log(listingError);
+    return false;
+  }
+
+  return true;
+}

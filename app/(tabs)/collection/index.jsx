2387f7730092a4b5aeccd6c64d558bdd3f4d5f44
Jose Kim <barsamic03@gmail.com>
2024-06-13 14:05:42 +0900

    Reconfigure file structure

--- app/(tabs)/collection/index.jsx
+++ app/(tabs)/collection/index.jsx
@@ -1,4 +1,4 @@
-import Collections from "../../components/tabs/Collections/Collections";
+import Collections from "../../../components/tabs/Collections/Collections";
 import useSession from "@/hooks/useSession";
 import Auth from "@/components/tabs/_common/Auth";
 import { useCallback, useEffect, useState } from "react";

1242cbcd616b6ec2317225b25156025d6e5b1176
Jose Kim <barsamic03@gmail.com>
2024-06-16 16:54:38 +0900

    Rename env variables to properly load them for testing

--- utils/supabase.js
+++ utils/supabase.js
@@ -2,8 +2,8 @@ import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
+const supabaseUrl = process.env[`EXPO_PUBLIC_SUPABASE_URL`];
+const supabaseKey = process.env[`EXPO_PUBLIC_SUPABASE_KEY`];
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {

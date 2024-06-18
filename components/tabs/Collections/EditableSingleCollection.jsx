09bf64c6c1e3a65e165ed4e919f12598fa4d4f90
Jose Kim <barsamic03@gmail.com>
2024-06-18 16:08:32 +0900

    Add testIDs and explanatory comments

--- components/tabs/Collections/EditableSingleCollection.jsx
+++ components/tabs/Collections/EditableSingleCollection.jsx
@@ -9,40 +9,78 @@ import {
 } from "react-native";
 import ItemCard from "@/components/tabs/_common/ItemCard"; // Import the ItemCard component
 
+/*
+The main component to be displayed on a single collection page
+which users can edit and remove items from
+
+Takes in the collection name,
+an array of item objects in that collection,
+and a callback function to be called when an item is deleted from collection
+*/
+
 export default function EditableSingleCollection({
   collectionName,
   collectionData,
   deleteFn,
 }) {
+  //State to store the data of the collection
   const [collection, setCollection] = useState(collectionData);
+
+  //State to store if the collection page is currently in delete mode
   const [deleteMode, setDeleteMode] = useState(false);
+
+  //State to store the items selected for deletion in delete mode
   const [selectedItems, setSelectedItems] = useState(new Set());
 
+  /*
+  Call useEffect every time the "collectionData" prop 
+  or the "collectionName" prop changes
+  to update state accordingly
+  */
   useEffect(() => {
     setCollection(collectionData);
   }, [collectionName, collectionData]);
 
+  //Function for toggling the delete mode of the collection
   const toggleDeleteMode = () => {
+    //Set the "deleteMode" state to be the opposite of what it was
     setDeleteMode(!deleteMode);
+
+    //Clear the items that were selected
+    //if deleteMode is changing from true to false
     if (deleteMode) {
       setSelectedItems(new Set());
     }
   };
 
+  //Function for selecting an item to be deleted
+  //and storing it inside the "selectedItems" set saved as state
   const selectItem = (id) => {
+    //Copy the selectedItems state as to not mutate existing state
     const newSelectedItems = new Set(selectedItems);
+
+    //If the given id is in selectedItems
+    //remove it
     if (selectedItems.has(id)) {
       newSelectedItems.delete(id);
     } else {
+      //otherwise add it
       newSelectedItems.add(id);
     }
+    //Update state
     setSelectedItems(newSelectedItems);
   };
 
+  //Function for deleting the selected items
   const handleConfirmDelete = () => {
+    //Call the deleteFn callback function
+    //for each of the item saved in the state to be deleted
+    //with the collectionName
     selectedItems.forEach((id) => {
       deleteFn(id, collectionName);
     });
+    //Update the local state
+    //to remove any items that were deleted as well
     const newItems = collection.filter(
       (item) => !selectedItems.has(item.ProductId)
     );
@@ -51,6 +89,8 @@ export default function EditableSingleCollection({
     setSelectedItems(new Set());
   };
 
+  //Function to display each items in the collection
+  //using the ItemCard
   const renderItem = ({ item }) => (
     <Pressable
       onPress={() => selectItem(item.ProductId)}
@@ -94,17 +134,25 @@ export default function EditableSingleCollection({
   );
 
   return (
-    <View style={styles.container}>
+    <View
+      style={styles.container}
+      testID="editable-single-collection-container"
+    >
       <View style={styles.headingContainer}>
         <Text style={styles.titleText}>{collectionName}</Text>
       </View>
       <View style={styles.pressableContainer}>
         {deleteMode && (
-          <Pressable title="Cancel" onPress={toggleDeleteMode}>
+          <Pressable
+            testID="editable-single-collection-cancel-button"
+            title="Cancel"
+            onPress={toggleDeleteMode}
+          >
             <Text style={styles.cancelDeleteText}>Cancel</Text>
           </Pressable>
         )}
         <Pressable
+          testID="editable-single-collection-edit-toggle-button"
           onPress={deleteMode ? handleConfirmDelete : toggleDeleteMode}
         >
           <Text style={styles.deleteToggleText}>
@@ -113,6 +161,7 @@ export default function EditableSingleCollection({
         </Pressable>
       </View>
       <FlatList
+        testID="editable-single-collection-item-list"
         data={collection}
         renderItem={renderItem}
         keyExtractor={(item) => item.ProductId}

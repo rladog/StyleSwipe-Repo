ccd1df4250714e361b182439e1c0f6be7ad07fc9
Jose Kim <barsamic03@gmail.com>
2024-06-20 17:01:53 +0900

    Create function for getting user's cart data

---
+++ utils/getCartData.js
@@ -0,0 +1,25 @@
+import { supabase } from "@/utils/supabase";
+import getUserId from "@/utils/getUserId";
+
+export default async function getCartData() {
+  const userId = await getUserId();
+  if (!userId) {
+    alert("Error getting login info");
+    console.log(error);
+    return null;
+  }
+
+  const { data, error } = supabase
+    .from("cart")
+    .select("cart_array")
+    .eq(id, userId)
+    .single();
+
+  if (error) {
+    alert("Error getting cart info");
+    console.log(error);
+    return null;
+  }
+
+  return data.cart_array;
+}

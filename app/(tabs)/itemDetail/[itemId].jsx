81758df543206d360d9444f19e3379b0fcc85e02
Jose Kim <barsamic03@gmail.com>
2024-06-21 16:31:10 +0900

    Pass in callback functions for item details page

--- app/(tabs)/itemDetail/[itemId].jsx
+++ app/(tabs)/itemDetail/[itemId].jsx
@@ -1,9 +1,11 @@
 import ItemDetailsPage from "@/components/tabs/Home/ItemDetailsPage";
 import { useCallback, useEffect, useState } from "react";
 import { useFocusEffect, useLocalSearchParams } from "expo-router";
-import getItemObjById from "../../../utils/getItemObjById";
+import getItemObjById from "@/utils/getItemObjById";
 import redirect from "@/utils/redirect";
 import LoadingScreen from "@/components/tabs/_common/LoadingScreen";
+import redirectToSeller from "@/utils/redirectToSeller";
+import addItemToCart from "@/utils/addItemToCart";
 
 export default function itemDetail() {
   const { itemId } = useLocalSearchParams();
@@ -24,5 +26,19 @@ export default function itemDetail() {
   if (!itemObj) {
     return <LoadingScreen loadingText={"Loading details..."} />;
   }
-  return <ItemDetailsPage itemObj={itemObj} closeFn={closeDetailsPage} />;
+  return (
+    <ItemDetailsPage
+      itemObj={itemObj}
+      closeFn={closeDetailsPage}
+      sellerFn={(itemObj) =>
+        redirectToSeller(
+          itemObj.is_listing,
+          itemObj.seller_contact,
+          itemObj.ProductTitle,
+          "https://nus.edu.sg/"
+        )
+      }
+      cartFn={(itemObj) => addItemToCart(itemObj.ProductId)}
+    />
+  );
 }

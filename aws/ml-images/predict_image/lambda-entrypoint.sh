98660fd7e7ccb10165e80cd42cf2e3a6d5a29d31
jonzyyyy <jonathan.ngien.zy@gmail.com>
2024-07-17 18:18:21 +0800

    Made changes to predict_image, now it can predict images accurately, but takes alot of time

--- aws/ml-images/predict_image/lambda-entrypoint.sh
+++ aws/ml-images/predict_image/lambda-entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Check if the first argument is provided
-if [ -z "$1" ]; then
-  echo "Handler name is required as the first argument"
-  exit 1
-fi
-
 # Print the handler name and arguments for debugging
 echo "Executing handler: $1 with function: $2"
 
